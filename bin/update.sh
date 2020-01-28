ca92a45f40add5c9eb3473c69b4e5e5ffe88a183
Katsumi Yagi <connvoykagi@gmail.com>
2020-01-28 12:49:53 +0900

    updated

--- bin/update.sh
+++ bin/update.sh
@@ -5,9 +5,12 @@ host=`hostname`
 if [ $host = "connvoidev"  ]
 then
     #echo "connvoi"
+    source /home/kyagi/pyenv/dev/bin/activate
     cd /home/kyagi/project/kiatsu/kiatsu-scripts/bin
 else
-    cd /Users/kyagi/Project/connvoi-service/kiatsu-scripts
+    source /Users/kyagi/Project/pyenv/data/bin/activate
+    cd /Users/kyagi/Project/connvoi-service/kiatsu-scripts/bin
 fi
 
 ./kiatsu.sh
+deactivate
